1ddaaa9e55aefad4ce80a89f6af47021f4cfcdd7
Pavan S Sabnis <pvnsbns@gmail.com>
2018-09-12 00:10:37 -0700

    Fitting simple linear regression to the training set

--- Machine Learning A-Z Template Folder/Part 2 - Regression/Section 4 - Simple Linear Regression/simple_linear_regression.py
+++ Machine Learning A-Z Template Folder/Part 2 - Regression/Section 4 - Simple Linear Regression/simple_linear_regression.py
@@ -11,22 +11,22 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 #import and section the datasets
-dataset = pd.read_csv('Data.csv')
+dataset = pd.read_csv('Salary_Data.csv')
 X = dataset.iloc[:, :-1].values
-Y = dataset.iloc[:, 3].values
+Y = dataset.iloc[:, 1].values
 
 
 
 # Splitting the dataset into the training set and the test set
 from sklearn.model_selection import train_test_split
 # Immediately define the variables required for the training and testing
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state = 42) 
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 1/3, random_state = 0) 
 # w/ test_size of 0.2 and 10 observations, 2 go to test & 8 go to training
 # X corresponds to the feature variables,
 # Y corresponds to the dependent variables
 # random_state is the seed of random # generator, can be any int value
 
-
+"""
 # Feature Scaling
 # import StandardScaler class for feature scaling
 from sklearn.preprocessing import StandardScaler
@@ -35,6 +35,17 @@ sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 # use sc_X to only transform corresponding test variable (features)
 X_test = sc_X.transform(X_test)
+"""
+
+''' feature scaling not required in linear regression because library for simple linear regression will
+take care of that for us '''
+
+# Fitting simple linear regression to the training set:
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, Y_train)
+# Now the model will learn the correlations of the training set once it is applied through the code above
+
 
 
 
