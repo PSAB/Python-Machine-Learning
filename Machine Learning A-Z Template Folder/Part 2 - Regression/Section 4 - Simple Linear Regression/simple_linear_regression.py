de09b17ee1e61b47a9272b0cd1b3419613ac5b36
Pavan S Sabnis <pvnsbns@gmail.com>
2018-09-11 17:35:10 -0700

    clear simple_linear_regression/py to set up for simple linear regression modeling

--- Machine Learning A-Z Template Folder/Part 2 - Regression/Section 4 - Simple Linear Regression/simple_linear_regression.py
+++ Machine Learning A-Z Template Folder/Part 2 - Regression/Section 4 - Simple Linear Regression/simple_linear_regression.py
@@ -1,47 +1,51 @@
-# Simple Linear Regression
+# #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 21 14:48:55 2018
 
-# Importing the libraries
+@author: johnyorangeseed
+"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Importing the dataset
-dataset = pd.read_csv('Salary_Data.csv')
+#import and section the datasets
+dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, 1].values
+Y = dataset.iloc[:, 3].values
 
-# Splitting the dataset into the Training set and Test set
-from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
+
+
+# Splitting the dataset into the training set and the test set
+from sklearn.model_selection import train_test_split
+# Immediately define the variables required for the training and testing
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state = 42) 
+# w/ test_size of 0.2 and 10 observations, 2 go to test & 8 go to training
+# X corresponds to the feature variables,
+# Y corresponds to the dependent variables
+# random_state is the seed of random # generator, can be any int value
+
 
 # Feature Scaling
-"""from sklearn.preprocessing import StandardScaler
+# import StandardScaler class for feature scaling
+from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
+# fit and transform the sc_X(StandardScaler) object onto training variable (features)
 X_train = sc_X.fit_transform(X_train)
+# use sc_X to only transform corresponding test variable (features)
 X_test = sc_X.transform(X_test)
-sc_y = StandardScaler()
-y_train = sc_y.fit_transform(y_train)"""
 
-# Fitting Simple Linear Regression to the Training set
-from sklearn.linear_model import LinearRegression
-regressor = LinearRegression()
-regressor.fit(X_train, y_train)
 
-# Predicting the Test set results
-y_pred = regressor.predict(X_test)
 
-# Visualising the Training set results
-plt.scatter(X_train, y_train, color = 'red')
-plt.plot(X_train, regressor.predict(X_train), color = 'blue')
-plt.title('Salary vs Experience (Training set)')
-plt.xlabel('Years of Experience')
-plt.ylabel('Salary')
-plt.show()
 
-# Visualising the Test set results
-plt.scatter(X_test, y_test, color = 'red')
-plt.plot(X_train, regressor.predict(X_train), color = 'blue')
-plt.title('Salary vs Experience (Test set)')
-plt.xlabel('Years of Experience')
-plt.ylabel('Salary')
-plt.show()
+
+
+
+
+
+
+
+
+
+
