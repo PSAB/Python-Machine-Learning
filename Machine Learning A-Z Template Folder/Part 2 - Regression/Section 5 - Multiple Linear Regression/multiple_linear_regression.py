7ced9a4b65140fc1aa0ada792f78158fcb7a7ed0
Pavan S Sabnis <pvnsbns@gmail.com>
2018-10-30 13:49:31 -0700

    Create new regressor for Backward Elimination

--- Machine Learning A-Z Template Folder/Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.py
+++ Machine Learning A-Z Template Folder/Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.py
@@ -21,7 +21,7 @@ X = onehotencoder.fit_transform(X).toarray()
 # Avoiding the dummy variable trap (deletes first column)
 X = X[:, 1:]
 # THis is taken care of in the background
-# But sometimes have to do it manually
+# But sometimes have to do it manually, it's a good practice
 
 
 # Splitting the dataset into the Training set and Test set
@@ -48,6 +48,20 @@ regressor.fit(X_train, y_train)
 
 # Predicting the test set results
 y_pred = regressor.predict(X_test)
+
+# Preparation of backward elmination
+
+# add a column of 1s to the (beginning 0f) independent data X 
+# aooend function from numpy is used to add on data
+import statsmodels.formula.api as sm
+X = np.append(arr = np.ones((50, 1)).astype(int), values = X, axis = 1)
+# Create new matrix of OPTIMAL features
+# Contains only independent variables that are statistically significant to the dependent variable
+# BACKWARD ELIMINATION STARTS HERE
+# Create new regressoe variable that utilizes statsmodels
+# OLS: Ordinary Least Squares
+X_opt = X[:, [0, 1, 2, 3, 4, 5]]
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
 
 
 
